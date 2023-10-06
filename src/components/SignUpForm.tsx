84559d8258452dc797e68d32141ed2cded02b2ff
Alevar <alevar03@mail.ru>
2023-10-06 07:57:44 +0300

    Added Formik to signup page

--- src/components/SignUpForm.tsx
+++ src/components/SignUpForm.tsx
@@ -1,61 +1,136 @@
-import { Stack, FormControl, Input, Button, Select } from "@chakra-ui/react";
+import {
+  Stack,
+  FormControl,
+  Input,
+  Button,
+  Select,
+  FormErrorMessage,
+} from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
+import { Formik, Form, Field } from "formik";
 
+interface FieldProps {
+  field: any;
+  form: any;
+}
 export default function () {
   const navigate = useNavigate();
   return (
-    <Stack as="form" spacing={4}>
-      <FormControl isRequired>
-        <Input
-          placeholder="Ваше имя"
-          focusBorderColor="orange.900"
-          borderColor="blue.400"
-        />
-      </FormControl>
-      <FormControl isRequired>
-        <Input
-          placeholder="Email для регистрации"
-          focusBorderColor="orange.900"
-          borderColor="blue.400"
-          type="email"
-        />
-      </FormControl>
-      <FormControl isRequired>
-        <Input
-          placeholder="Надежный пароль"
-          type="password"
-          focusBorderColor="orange.900"
-          borderColor="blue.400"
-        />
-      </FormControl>
-      <FormControl isRequired>
-        <Input
-          placeholder="Повторите пароль"
-          type="password"
-          focusBorderColor="orange.900"
-          borderColor="blue.400"
-        />
-      </FormControl>
-      <FormControl isRequired>
-        <Select
-          placeholder="Кто вы?"
-          focusBorderColor="orange.900"
-          borderColor="blue.400"
-        >
-          <option value="customer">Я закачик</option>
-          <option value="freelancer">Я Фрилансер</option>
-        </Select>
-      </FormControl>
-      <Button colorScheme="buttonOrange" type="submit" mt={4}>
-        Зарегистрироваться
-      </Button>
-      <Button
-        colorScheme="orange"
-        variant="outline"
-        onClick={() => navigate("/login")}
-      >
-        Вход
-      </Button>
-    </Stack>
+    <Formik
+      initialValues={{
+        name: "",
+        email: "",
+        password: "",
+        repeatPassword: "",
+        role: "",
+      }}
+      onSubmit={(values, actions) => {
+        alert(JSON.stringify(values, null, 2));
+        actions.setSubmitting(false);
+      }}
+    >
+      {(props) => (
+        <Form>
+          <Stack spacing={4}>
+            <Field name="name">
+              {({ field, form }: FieldProps) => (
+                <FormControl
+                  isRequired
+                  isInvalid={form.errors.name && form.touched.name}
+                >
+                  <Input
+                    {...field}
+                    placeholder="Ваше имя"
+                    focusBorderColor="orange.900"
+                    borderColor="blue.400"
+                  />
+                  <FormErrorMessage>{form.errors.name}</FormErrorMessage>
+                </FormControl>
+              )}
+            </Field>
+            <Field name="email" type="email">
+              {({ field, form }: FieldProps) => (
+                <FormControl
+                  isRequired
+                  isInvalid={form.errors.name && form.touched.name}
+                >
+                  <Input
+                    {...field}
+                    placeholder="Email для регистрации"
+                    focusBorderColor="orange.900"
+                    borderColor="blue.400"
+                    type="email"
+                  />
+                  <FormErrorMessage>{form.errors.name}</FormErrorMessage>
+                </FormControl>
+              )}
+            </Field>
+            <Field name="password">
+              {({ field, form }: FieldProps) => (
+                <FormControl
+                  isRequired
+                  isInvalid={form.errors.name && form.touched.name}
+                >
+                  <Input
+                    {...field}
+                    placeholder="Надежный пароль"
+                    type="password"
+                    focusBorderColor="orange.900"
+                    borderColor="blue.400"
+                  />
+                  <FormErrorMessage>{form.errors.name}</FormErrorMessage>
+                </FormControl>
+              )}
+            </Field>
+            <Field name="repeatPassword">
+              {({ field, form }: FieldProps) => (
+                <FormControl
+                  isRequired
+                  isInvalid={form.errors.name && form.touched.name}
+                >
+                  <Input
+                    {...field}
+                    placeholder="Повторите пароль"
+                    type="password"
+                    focusBorderColor="orange.900"
+                    borderColor="blue.400"
+                  />
+                  <FormErrorMessage>{form.errors.name}</FormErrorMessage>
+                </FormControl>
+              )}
+            </Field>
+            <Field name="role">
+              {({ field, form }: FieldProps) => (
+                <FormControl
+                  isRequired
+                  isInvalid={form.errors.name && form.touched.name}
+                >
+                  <Select
+                    {...field}
+                    placeholder="Кто вы?"
+                    focusBorderColor="orange.900"
+                    borderColor="blue.400"
+                  >
+                    <option value="customer">Я закачик</option>
+                    <option value="freelancer">Я Фрилансер</option>
+                  </Select>
+                  <FormErrorMessage>{form.errors.name}</FormErrorMessage>
+                </FormControl>
+              )}
+            </Field>
+            <Button colorScheme="buttonOrange" type="submit" mt={4}>
+              Зарегистрироваться
+            </Button>
+            <Button
+              colorScheme="orange"
+              variant="outline"
+              onClick={() => navigate("/login")}
+            >
+              Вход
+            </Button>
+          </Stack>
+        </Form>
+      )}
+    </Formik>
   );
 }
